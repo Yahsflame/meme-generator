b527ce82f75a07daad7ee2932a90ca0c118abd43
Ben Sigal <ben.sigal@bookelevent.com>
2023-12-07 09:19:18 -0700

    completed basic requirements.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
+// App.tsx
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ImageEditor from './ImageEditor';
 
-function App() {
+const App: React.FC = () => {
+  const handleImageChange = (url: string) => {
+    // Handle image changes if needed
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Meme Generator</h1>
+      <ImageEditor onImageChange={handleImageChange} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/OutputArea.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Area } from 'react-easy-crop/types';
+
+interface OutputAreaProps {
+  crop: Area;
+  rotation: number;
+}
+
+const OutputArea: React.FC<OutputAreaProps> = ({ crop, rotation }) => {
+  return (
+    <div>
+      <h3>Live Output:</h3>
+      <p>Crop: {JSON.stringify(crop)}</p>
+      <p>Rotation: {rotation}</p>
+      {/* Add additional output information as needed */}
+    </div>
+  );
+};
+
+export default OutputArea;
